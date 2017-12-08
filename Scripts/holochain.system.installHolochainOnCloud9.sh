8d7de2419d41c866fa86337cc4aecf9128e1e816
Jay DayZee <christopherreay@gmail.com>
2017-12-08 20:46:54 +0000

    fixes to installation Script

--- Scripts/holochain.system.installHolochainOnCloud9.sh
+++ Scripts/holochain.system.installHolochainOnCloud9.sh
@@ -10,12 +10,14 @@ else
     
     mkdir -p ~/workspace/context/reality/programming/golang
     cd ~/workspace/context/reality/programming/golang
-    wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+    [ -f ~/workspace/context/reality/programming/golang/go1.9.2.linux-amd64.tar.gz ] || wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
     mkdir ~/workspace/local
     tar -C ~/workspace/local/ -xf go1.9.2.linux-amd64.tar.gz
     
+    . ~/workspace/Scripts/holochain.system.configureLocalGo.sh
+    
     go get -d -v github.com/metacurrency/holochain
-    cd go/src/github.com/metacurrency/holochain
+    cd ~/workspace/go/src/github.com/metacurrency/holochain
     git checkout develop
     make deps
     make
@@ -27,7 +29,10 @@ else
     
     touch ~/workspace/Scripts/holochain.system.holochainHasBeenInstalled
     
+    cd ~/workspace
+    
     echo "♓: HolochainOnCloud9 super local go is installed"
+    
 fi
 
 if grep -q "holochain.314" ~/.profile ; then
@@ -36,7 +41,7 @@ if grep -q "holochain.314" ~/.profile ; then
         echo "♓: HolochainOnCloud9 installed correctly"
     else
         echo "♓: HolochainOnCloud9 appears to be missinstalled. Please run:"
-        echo "source ~/Scripts/holochain.system.configureLocalGo.sh"
+        echo "source ~/workspace/Scripts/holochain.system.configureLocalGo.sh"
         exit 1
     fi 
 else
@@ -50,7 +55,7 @@ if grep -q "holochain.315" ~/.profile ; then
         echo "♓: HolochainOnCloud9 path is set correctly"
     else
         echo "♓: HolochainOnCloud9 appears to be missinstalled. Please run:"
-        echo "source ~/Scripts/holochain.system.configureLocalGo.sh"
+        echo "source ~/workspace/Scripts/holochain.system.configureLocalGo.sh"
         exit 1
     fi
 else
